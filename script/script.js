ef3e158b2a1ee3c9e3a7c13cdd3bc103936c1efc
Alexey Pogodin <alex.pogodin.jr@gmail.com>
2021-06-26 22:13:10 +0300

    Fixed some bugs with display on smartphones and added on-close animation in user-form

--- script/script.js
+++ script/script.js
@@ -1,32 +1,34 @@
-let userDataFormClose1 = document.getElementById('close-form1');
-let userDataFormClose2 = document.getElementById('close-form2');
-let userDataForm = document.getElementById('user-data');
+const userDataFormClose1 = document.getElementById('close-form1');
+const userDataForm = document.getElementById('user-data');
 
-let closeForm = function() {
-    userDataForm.classList.toggle('_form-closed');
-    userDataForm.classList.toggle('_form-opened');
-}
-let openForm = function() {
-    userDataForm.classList.toggle('_form-closed');
-    userDataForm.classList.toggle('_form-opened');
-}
-
+userDataForm.classList.add('_form-closed');
+userDataFormClose1.classList.add('_icon-closed');
 
 userDataForm.opened = true;
-userDataForm.close = closeForm;
-userDataForm.open = openForm;
-
-formHideShow = function () {
+userDataForm.openClose = function() {
+    userDataForm.classList.toggle('_form-closed');
+    userDataForm.classList.toggle('_form-opened');
     if (userDataForm.opened) {
-        userDataForm.close();
-        console.log('closed')
-    } else {
-        userDataForm.open();
-        console.log('opened')
-    }
+        userDataForm.opened = false;
+    } else { userDataForm.opened = false;}
 }
 
-userDataFormClose1.onclick = formHideShow;
-userDataFormClose1.onclick = formHideShow;
-userDataFormClose2.onclick = formHideShow;
-userDataFormClose2.onclick = formHideShow;
+formHideShow = function () {
+        userDataForm.openClose();
+}
+formCircleChange = function () {
+    userDataFormClose1.classList.toggle('_anim-rotation');
+    setTimeout(function () {
+        userDataFormClose1.classList.toggle('_icon-opened');
+        userDataFormClose1.classList.toggle('_icon-closed');
+        userDataFormClose1.classList.toggle('_anim-rotation');
+    }, 500);
+}
+
+formAction = function () {
+    formHideShow();
+    formCircleChange();
+}
+
+userDataFormClose1.onclick = formAction;
+userDataFormClose1.onclick = formAction;
